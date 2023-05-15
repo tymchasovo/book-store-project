22ad667dec8707ba5f2797d9a7f9934b122b89db
Irina Savina <irinaskchva2@gmail.com>
2023-05-15 23:44:26 +0300

    book-store-test

---
+++ home.py
@@ -0,0 +1,35 @@
+# # Adding a comment
+#
+# from selenium import webdriver
+#
+# driver = webdriver.Chrome()
+# driver.maximize_window()
+# driver.implicitly_wait(10)
+#
+# # Open link
+# driver.get("https://practice.automationtesting.in/")
+# # Scrolling down the page
+# driver.execute_script("window.scrollBy(0, 600);")
+# # Clicking on the title of the book "Selenium Ruby"
+# selenium_ruby_book = driver.find_element_by_css_selector('[title="Selenium Ruby"]')
+# selenium_ruby_book.click()
+# # Going to the "REVIEWS" tab
+# driver.execute_script("window.scrollBy(0, 300);")
+# reviews_btn = driver.find_element_by_css_selector("li.reviews_tab")
+# reviews_btn.click()
+# # Setting 5 stars
+# driver.execute_script("window.scrollBy(0, 600);")
+# all_stars = driver.find_element_by_css_selector("a.star-5")
+# all_stars.click()
+# # Filling in the fields "Review", "Name", "Email"
+# review_field = driver.find_element_by_name("comment")
+# review_field.send_keys("Nice Book!")
+# name_field = driver.find_element_by_css_selector("input#author")
+# name_field.send_keys("Irene")
+# email_field = driver.find_element_by_css_selector("input#email")
+# email_field.send_keys("test22@gmail.com")
+# # Clicking on the "SUBMIT" button
+# submit_btn = driver.find_element_by_class_name("submit")
+# submit_btn.click()
+#
+# driver.quit()
---
+++ login_registration.py
@@ -0,0 +1,62 @@
+# # Registration
+#
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+#
+# driver = webdriver.Chrome()
+# driver.maximize_window()
+# driver.implicitly_wait(10)
+#
+# # Open link
+# driver.get("https://practice.automationtesting.in/")
+# # Going to the "My Account" section
+# my_account_btn = driver.find_element_by_link_text("My Account")
+# my_account_btn.click()
+# # Email for registration
+# email_field = driver.find_element_by_css_selector("input#reg_email")
+# email_field.send_keys("test02@gmail.com")
+# # Password for registration
+# password_field = driver.find_element_by_css_selector("input#reg_password")
+# password_field.send_keys("i-tymchasovo4")
+# # Clicking on the "Register" button
+# register_btn = WebDriverWait(driver, 10).until(
+#     EC.element_to_be_clickable((By.NAME, "register")))
+# register_btn.click()
+#
+# driver.quit()
+
+
+
+# # Login
+#
+# from selenium import webdriver
+# from selenium.common.exceptions import NoSuchElementException
+#
+# driver = webdriver.Chrome()
+# driver.maximize_window()
+# driver.implicitly_wait(10)
+#
+# # Open link
+# driver.get("https://practice.automationtesting.in/")
+# # Going to the "My Account" section
+# my_account_btn = driver.find_element_by_link_text("My Account")
+# my_account_btn.click()
+# # Email for login
+# email_field = driver.find_element_by_id("username")
+# email_field.send_keys("test02@gmail.com")
+# # Password for login
+# password_field = driver.find_element_by_id("password")
+# password_field.send_keys("i-tymchasovo4")
+# # Clicking on the "Login" button
+# login_btn = driver.find_element_by_name("login")
+# login_btn.click()
+# # Checking the "Logout" element on the page
+# try:
+#     logout = driver.find_element_by_link_text("Logout")
+#     print("Элемент Logout присутствует на странице")
+# except NoSuchElementException:
+#     print("Элемент Logout отсутствует")
+#
+# driver.quit()
